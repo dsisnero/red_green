bd8de9ec5720a436b1bf9c65aa9f5689f6cbbd67
dsisnero <dsisnero@gmail.com>
2026-02-02 10:38:27 -0700

    Add green node base class

--- src/red_green.cr
+++ src/red_green.cr
@@ -1,4 +1,6 @@
 # RedGreen provides the core namespace for the Roslyn red/green tree port.
+require "./red_green/green_node"
+
 module RedGreen
   VERSION = "0.1.0"
 end
---
+++ src/red_green/green_node.cr
@@ -0,0 +1,28 @@
+module RedGreen
+  # Immutable, position-less syntax node (green tree).
+  abstract class GreenNode
+    SLOT_COUNT_MASK = 0x000F_u16
+    FLAGS_SHIFT     =          4
+
+    getter kind : UInt16
+    getter full_width : Int32
+
+    def initialize(@kind : UInt16, @flags_and_slot_count : UInt16, @full_width : Int32)
+    end
+
+    def slot_count : Int32
+      (@flags_and_slot_count & SLOT_COUNT_MASK).to_i32
+    end
+
+    def flags_bits : UInt16
+      (@flags_and_slot_count >> FLAGS_SHIFT).to_u16
+    end
+
+    abstract def language : String
+    abstract def kind_text : String
+    abstract def token? : Bool
+    abstract def trivia? : Bool
+    abstract def get_slot(index : Int32) : GreenNode?
+    abstract def create_red(parent, position : Int32)
+  end
+end
